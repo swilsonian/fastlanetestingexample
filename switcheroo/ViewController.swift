efee7828fef3736b939fc87234bad75e76523207
swilson <swilson@swilsons-MacBook-Pro.local>
2015-07-29 14:50:01 +0800

    Initial Commit

---
+++ switcheroo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  switcheroo
+//
+//  Created by swilson on 7/29/15.
+//  Copyright (c) 2015 swilson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
